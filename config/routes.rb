394953c4fef78b4cd7f45850d631184dd514b1c6
keisuketomita <keisuke.tommy@mail.com>
2021-02-15 19:32:32 +0900

    create follow function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   root 'cinemas#index'
   # root to: 'users#show'
+  resources :relationships, only: [:create, :destroy]
   devise_for :users
   resources :cinemas
   resources :users, only: [:index, :new, :edit, :update, :show]
